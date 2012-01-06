a50653e3fab8e76a9c5dfb5e63face46edc9a77f
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2012-01-06 17:54:17 +0530

    Fix the invisible/requried property being set on entry itself

--- purchase.py
+++ purchase.py
@@ -2,6 +2,7 @@
 #this repository contains the full copyright notices and license terms.
 
 from trytond.model import ModelSQL, ModelView, fields
+from trytond.pool import Pool
 from trytond.pyson import Eval, Equal, Not
 
 
@@ -11,13 +12,15 @@ class PurchaseLine(ModelSQL, ModelView):
     """
     _name = 'purchase.line'
 
-    product_fleet_type = fields.Function(fields.Char('Product_Fleet_Type'),
-        'get_product_fleet_type')
+    product_fleet_type = fields.Function(
+        fields.Char('Product_Fleet_Type', on_change_with=['product']),
+        'get_product_fleet_type'
+        )
 
     asset = fields.Many2One("fleet.asset", "Asset",
         states={
             'invisible': Not(Equal(Eval('product_fleet_type'), 'fuel')),
-            'required': (Eval('product_fleet_type') == 'fuel')
+            'required': Equal(Eval('product_fleet_type'), 'fuel')
             },
         depends=['product_fleet_type']
         )
@@ -37,5 +40,15 @@ class PurchaseLine(ModelSQL, ModelView):
         for purchase_line in self.browse(ids):
             res[purchase_line.id] = purchase_line.product.fleet_management_type
         return res
+
+    def on_change_with_product_fleet_type(self, vals):
+        """Set the value of product_fleet_type so that the invisible and 
+        required property may be set
+        """
+        product_obj = Pool().get('product.product')
+        if vals.get('product'):
+            product = product_obj.browse(vals['product'])
+            return product.fleet_management_type
+        return None
 
 PurchaseLine()
